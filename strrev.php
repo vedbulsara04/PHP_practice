9f25a805a7caac4c066b83af9c74cf6376b66ba2
Ved Bulsara <vedbulsara9@gmail.com>
2022-08-27 17:55:42 +0530

    1st commit(27/8/22)

---
+++ strrev.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>String methods</title>
+  </head>
+
+  <body>
+    <?php
+        $str = "racecar";
+        $str2 = "Mercedes AMG Formula 1";
+          echo strrev($str);
+          echo "<br><br>";
+          echo strrev($str2);
+    ?>  
+  </body>
+
+</html>
