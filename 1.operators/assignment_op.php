6e0c78eb906eac38d730be3c23668d64bf81c065
Ved Bulsara <vedbulsara7@gmail.com>
2022-08-29 07:41:48 +0530

    6th commit(29/8/22)

---
+++ 1.operators/assignment_op.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Assignment Operator</title>
+  </head>
+  
+  <body>
+    <?php
+
+        $x = 7;
+            $x+=93;
+            echo "Addition : $x";
+                        echo "<br> <br>";
+        $y = 105;
+            $y-=5;
+            echo "Subtraction : $y";
+                        echo "<br> <br>";
+        $a = 2;
+            $a*=5;
+            echo "Multiplcation : $a";
+                        echo "<br> <br>";
+        $b = 4;
+            $b/=2;
+            echo "Division : $b";
+                        echo "<br> <br>";
+        $c = 6;
+            $c%=4;
+            echo "Modulus : $c";
+                        echo "<br> <br>";
+    ?>
+  </body>
+
+</html>
